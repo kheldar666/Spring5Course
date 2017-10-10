75fef91cabb215572fc34eebd61936ae472eb397
Martin Papy <martin.papy@gmail.com>
2017-10-10 09:54:57 +0700

    Adding some configuration

--- chuck-jokes/src/main/java/org/libermundi/chuck/ChuckJokesApplication.java
+++ chuck-jokes/src/main/java/org/libermundi/chuck/ChuckJokesApplication.java
@@ -2,8 +2,10 @@ package org.libermundi.chuck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChuckJokesApplication {
 
 	public static void main(String[] args) {
